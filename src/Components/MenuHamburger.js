0afa7beb7cc47e54fe407525d673ea799857bd37
Centaur Games <tech.support@centaurgamesonline.com>
2022-06-15 12:55:23 -0700

    feat: added responsive menu hamburger

---
+++ src/Components/MenuHamburger.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { slide as Menu } from "react-burger-menu";
+import { LIST_PAGE_META_INFO } from "../Util/SiteUtil";
+import { BASE_URL } from "../Util/UrlHelper";
+
+export class MenuHamburger extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="menu-hamburger-placeholder" />
+                <div className="menu-hamburger">
+                    <Menu>
+                        {Object.keys(LIST_PAGE_META_INFO).map(listPageName => (
+                            <a href={BASE_URL + "?pageType=list&pageName=" + listPageName} key={listPageName} className="menu-bar-text">
+                                {listPageName}
+                            </a>
+                        ))}
+                    </Menu>
+                </div>
+            </div>
+        );
+    }
+}
