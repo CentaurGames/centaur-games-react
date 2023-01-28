df8352b81ec283abdcedd31cee4e76eab1503fe1
Centaur Games <tech.support@centaurgamesonline.com>
2023-01-28 13:26:20 -0800

    test: added test coverage for BlogListPage component

---
+++ src/Components/BlogListPage.jest.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import {cleanup, render} from "@testing-library/react";
+import { BlogListPage } from "./BlogListPage";
+import { BLOG_META_INFO } from "../Util/SiteUtil";
+
+describe("BlogListPage component", () => {
+    afterEach(() => {
+        cleanup();
+    });
+
+    it("displays correctly", () => {
+        const {getByText} = render(
+            <BlogListPage />
+        );
+
+        for (const blogName in BLOG_META_INFO) {
+            const {title} = BLOG_META_INFO[blogName];
+            const blogTitle = getByText(title);
+            expect(blogTitle).not.toBeNull();
+        }
+    });
+});
