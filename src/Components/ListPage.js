d0f057bbd054de2cc722c8f1452bc59e6fd7f21a
Centaur Games <tech.support@centaurgamesonline.com>
2023-02-01 13:20:29 -0800

    fixed styling caused by upgrades

--- src/Components/ListPage.js
+++ src/Components/ListPage.js
@@ -37,7 +37,7 @@ export class ListPage extends React.Component {
         </div>
         <div className="list-page">
           <div className="wrap-div">
-            <Carousel>
+            <Carousel indicators={false}>
               <Carousel.Item>
                 <img
                   src={LIST_PAGE_META_INFO[this.props.name].sliderImageURL}
