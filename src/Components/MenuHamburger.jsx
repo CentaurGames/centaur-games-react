c043cdc5bb7da99b775eacdf1ec8dc505937ffb2
Centaur Games <tech.support@centaurgamesonline.com>
2023-02-07 18:15:11 -0800

    filtered menu hamburger by android

--- src/Components/MenuHamburger.jsx
+++ src/Components/MenuHamburger.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { slide as Menu } from "react-burger-menu";
-import { LIST_PAGE_META_INFO } from "../Util/SiteUtil";
+import { IS_ANDROID_SUNSET, LIST_PAGE_META_INFO } from "../Util/SiteUtil";
 import { BASE_URL } from "../Util/UrlHelper";
 
 export function MenuHamburger() {
@@ -9,7 +9,7 @@ export function MenuHamburger() {
       <div className="menu-hamburger-placeholder" />
       <div className="menu-hamburger">
         <Menu>
-          {Object.keys(LIST_PAGE_META_INFO).map((listPageName) => (
+          {Object.keys(LIST_PAGE_META_INFO).filter((key) => !IS_ANDROID_SUNSET || key !== "Android").map((listPageName) => (
             <a
               href={BASE_URL + "?pageType=list&pageName=" + listPageName}
               key={listPageName}
